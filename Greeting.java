f1f02c6c05e0b1136ff4915a06b8d4980a4fbfa7
mtsuka1999 <53078017+mtsuka1999@users.noreply.github.com>
2020-08-06 22:04:46 +0900

    Update and rename OracleTest.java to Greeting.java
    User name added

---
+++ Greeting.java
@@ -0,0 +1,15 @@
+
+class Greeting {
+    public static void main(String[] args) {
+      public static void main(String args[]){
+    System.out.println("Who are you? ");
+    System.out.println("<")
+
+    Scanner scan = new Scanner(System.in);
+
+    String str = scan.next();
+    System.out.println("Hello, "+ str +"!");
+
+   
+  }
+}
